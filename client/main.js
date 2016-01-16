2dce67dc87403d26b6091e95b064f6695d23f55b
Maks Trawinski <maks.trawinski@icloud.com>
2016-01-16 17:53:15 +0100

    layout injection in main.js and rout modularisation

--- client/main.js
+++ client/main.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import {injectDeps} from 'react-simple-di';
+
 import loadMethodStubs from './configs/method_stubs';
 import {initContext} from './configs/context';
+
 import {initRoutes} from './configs/routes.jsx';
-import actions from './actions';
+// import actions from './actions';
+import actions from './actionsCombined.js';
+
+import {initRoutesInspinia} from './modules/inspinia/routesFR.jsx';
+import {initRoutesHome} from './modules/_home/routesFR.jsx';
+
+import Layout from '/client/modules/inspinia/components/Layout.jsx';
 
 loadMethodStubs();
 const context = initContext();
-initRoutes(context, actions);
+const LayoutCtx = injectDeps(context, actions)(Layout);
+// initRoutes(context, actions);
+initRoutes(LayoutCtx);
+initRoutesHome(LayoutCtx);
+// initRoutesInspinia(context, actions);
