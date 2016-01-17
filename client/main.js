2b90a5ea5808976161c2600316e0d07547fffc41
Maks Trawinski <maks.trawinski@icloud.com>
2016-01-17 15:01:13 +0100

    update main.js to use new routes

--- client/main.js
+++ client/main.js
@@ -5,18 +5,21 @@ import loadMethodStubs from './configs/method_stubs';
 import {initContext} from './configs/context';
 
 import {initRoutes} from './configs/routes.jsx';
-// import actions from './actions';
+
 import actions from './actionsCombined.js';
 
-import {initRoutesInspinia} from './modules/inspinia/routesFR.jsx';
-import {initRoutesHome} from './modules/_home/routesFR.jsx';
+import initRoutesHome from './modules/_home/routesFR.jsx';
+import initRoutesUsers from './modules/_users/routesFR.jsx';
+import initRoutesColors from './modules/colors/routesFR.jsx';
+import initRoutesInspinia from './modules/inspinia/routesFR.jsx';
 
 import Layout from '/client/modules/inspinia/components/Layout.jsx';
 
 loadMethodStubs();
 const context = initContext();
 const LayoutCtx = injectDeps(context, actions)(Layout);
-// initRoutes(context, actions);
+
 initRoutes(LayoutCtx);
 initRoutesHome(LayoutCtx);
-// initRoutesInspinia(context, actions);
+initRoutesUsers(LayoutCtx);
+initRoutesColors(LayoutCtx);
