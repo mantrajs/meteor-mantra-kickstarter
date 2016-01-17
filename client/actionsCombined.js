8167d783046969b4c948d5e39af442fcaf0fe84a
Maks Trawinski <maks.trawinski@icloud.com>
2016-01-17 15:01:56 +0100

    aggregate new actions

--- client/actionsCombined.js
+++ client/actionsCombined.js
@@ -1,7 +1,11 @@
 import posts from './actions/posts';
-import home from './modules/_home/actions.js';
+import _home from './modules/_home/actions.js';
+import _users from './modules/_users/actions.js';
+import colors from './modules/colors/actions.js';
 
 export default {
   posts
-  ,home
+  ,_home
+  ,_users
+  ,colors
 };
