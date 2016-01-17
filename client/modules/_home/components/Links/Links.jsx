8bb678da4baa25e52d01b93fba7762b4ca589b78
Maks Trawinski <maks.trawinski@icloud.com>
2016-01-17 19:31:51 +0100

    update to admin LTE

--- client/modules/_home/components/Links/Links.jsx
+++ client/modules/_home/components/Links/Links.jsx
@@ -1,8 +1,9 @@
 
 import React from 'react';
-import Dropdown from '/client/modules/inspinia/components/Dropdown.jsx';
-export default () => {
+// import Dropdown from '/client/modules/inspinia/components/Dropdown.jsx';
+import Dropdown from '../../../AdminLTE/components/Dropdown.jsx';
 
+export default () => {
 
   var links = [];
   links.push({url:'/', name: 'Start'});
@@ -25,18 +26,21 @@ export default () => {
   links.push( {url:'/login', name: 'Login'} );
   links.push( {url:'/password', name: 'Forgot passoword'} );
   links.push( {url:'/register', name: 'Register'} );
+  links.push( {url:'/Logout', name: 'Logout'} );
 
   links.push( {url:'/account', name: 'Account'} );
-  links.push( {url:'/profile', name: 'Logout'} );
-  
+  links.push( {url:'/profile', name: 'Profile'} );
+
   let navBlaze = React.createElement(Dropdown, {name: 'Accounts', links: links});
 
 
   return (
     <ul className="nav navbar-nav">
+
       { navStart }
       { navColor }
       { navBlaze }
+
     </ul>
   );
 };
