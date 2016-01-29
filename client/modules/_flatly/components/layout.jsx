0b90a16d65a61f2182e327d867f40daf3b92c147
Maks <maks@collabtive.net>
2016-01-29 23:58:48 +0100

    initial _flatly theme import

---
+++ client/modules/_flatly/components/layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// import Navheader from './navigation2.jsx';
+import Navheader from './navheader.jsx';
+
+export default class extends React.Component {
+
+  render () {
+    return (
+      <div>
+
+        <Navheader>
+          {this.props.links ? this.props.links() : ''}
+        </Navheader>
+
+        <div className="container">
+          {this.props.content()}
+        </div>
+
+      </div>
+    )
+  }
+}
---
+++ client/modules/_flatly/components/pageheader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class extends React.Component {
+
+  render () {
+    return (
+      <div className="page-header" id="banner">
+        <div className="row">
+          <div className="col-lg-6">
+            <h1>Welcome</h1>
+            <p className="lead">To a new bootstrap theme by UseBootstrap.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
