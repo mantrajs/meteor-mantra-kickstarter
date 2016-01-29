57fca595dc8a408763d7c24fce9ecbf2c8e8cda3
Maks <maks@collabtive.net>
2016-01-29 20:27:55 +0100

    add server methods and initial users

---
+++ server/configs/initial_users.js
@@ -0,0 +1,16 @@
+export default () => {
+
+  if (Meteor.users.find().count() == 0 ){
+    console.log ('lets create some users');
+
+    const first_id  = Accounts.createUser({
+      email: 'test@test.com'
+      ,password: 'test1234'
+      // ,profile: {
+      //   firstName: 'Austin',
+      //   lastName: 'Powers',
+      // }
+    });
+
+  }
+}
