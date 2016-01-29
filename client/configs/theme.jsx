658f76eaaf114894b0bb83c856097a2ba65fe8b8
Maks <maks@collabtive.net>
2016-01-29 20:49:13 +0100

    add client theme config

---
+++ client/configs/theme.jsx
@@ -0,0 +1,4 @@
+// import Layout from '/client/modules/_home/components/layoutJustContent.jsx';
+import Layout from '/client/modules/_adminLTE/components/layout.jsx';
+import Dropdown from '/client/modules/_adminLTE/components/Dropdown.jsx';
+export {Layout, Dropdown}
