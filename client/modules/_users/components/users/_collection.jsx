af3391162dbae213f92ce7f819dca94df4af1e8c
Greg Stephens <greg@udon.org>
2016-03-01 07:28:19 -0800

    User collection table

--- client/modules/_users/components/users/_collection.jsx
+++ client/modules/_users/components/users/_collection.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 
+function deleteUser (id) {
+    console.log('deleteUser: ' + id);
+    //this.props.deleteAction(id);
+}
+
 export default ({collection}) => (
   <div>
-    <h3>Collection</h3>
-    <ul>
-      {collection.map(record => (
-        <li key={record._id}>
-          <a href={`/users/${record._id}`}>{record.firstEmail()}</a>
-        </li>
-      ))}
-    </ul>
+    <h3>Users</h3>
+      <table className="table table-responsive table-striped table-hover table-condensed">
+          <thead>
+          <tr>
+              <th className="col-xs-4">email</th>
+              <th className="col-xs-2">First Name</th>
+              <th className="col-xs-2">Last Name</th>
+              <th className="col-xs-4">Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          {collection.map(record => (
+              <tr className="text-left" key={record._id}>
+                  <td className="col-xs-4"><a href={`/users/${record._id}`}>{record.firstEmail()}</a></td>
+                  <td className="col-xs-2">{record.profile.firstName}</td>
+                  <td className="col-xs-2">{record.profile.lastName}</td>
+                  <td className="col-xs-4">
+                      <a className="btn btn-default btn-sm" role="button" href={`/users/${record._id}`}>View</a>
+                      <a className="btn btn-default btn-sm" role="button" href={`/users/${record._id}/edit`}>Edit</a>
+                      <button className="btn btn-default btn-sm" role="button" onClick={() => deleteUser(record._id)}>Delete</button>
+                  </td>
+              </tr>
+          ))}
+          </tbody>
+      </table>
   </div>
 );
