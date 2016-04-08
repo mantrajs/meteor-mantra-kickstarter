f61924f4bc7955aa23009fc80b013c6a3bae63e2
Max Trawinski <max@collabtive.net>
2016-04-08 21:12:51 +0200

    add module template

---
+++ client/modules/_module_template/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {mount} from 'react-mounter';
+
+
+import {
+  AuthCheck,
+  LayoutDefault,
+  Simplest,
+} from '/client/configs/components.js';
+
+export default function (injectDeps, {FlowRouter}) {
+
+  const AuthCheckCtx = injectDeps(AuthCheck);
+
+  FlowRouter.route('/template', {
+    name: 'template.route',
+    action() {
+      mount(AuthCheckCtx, {
+        LayoutDefault, content: () => (<Simplest title="Template..."/>)
+      });
+    }
+  });
+  
+}
