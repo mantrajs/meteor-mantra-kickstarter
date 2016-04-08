5c02a987e3379a3871393ba52cc99e9894ed0868
Max Trawinski <max@collabtive.net>
2016-04-08 11:49:10 +0200

    update config files

---
+++ client/configs/components.js
@@ -0,0 +1,14 @@
+
+import LayoutDefault from '/client/modules/themeflatly/components/Layout.jsx';
+
+import AuthCheck from '/client/modules/app/components/AuthCheck/Wrapper.jsx';
+import Simplest from '/client/modules/app/components/Simplest.jsx';
+import NotFound from '/client/modules/app/components/NotFound/Wrapper.jsx';
+
+
+export {
+  AuthCheck,
+  LayoutDefault,
+  Simplest,
+  NotFound
+};
---
+++ client/configs/composers.js
@@ -0,0 +1,13 @@
+
+import authComposer from '/client/modules/app/lib/Auth.js';
+import {_authComposer, _authDepsMapper} from '/client/modules/app/lib/Auth.js';
+import navHeaderComposer from '/client/modules/app/lib/NavHeader.js';
+
+export {
+
+  authComposer,
+  _authComposer,
+  _authDepsMapper,
+  navHeaderComposer,
+
+};
